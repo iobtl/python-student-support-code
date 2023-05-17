5b96a73bf4feb31c8bad5e60ef752877c0d84f8f
iobtl <tedmundhtl@gmail.com>
2023-05-17 12:08:02 +0800

    x86var: select_instructions passing
    Need to simplify select_stmt.

--- compiler.py
+++ compiler.py
@@ -24,13 +24,16 @@ class Compiler:
                 i = Name(generate_name("input"))
                 return (i, [(i, Call(Name("input_int"), []))])
             case UnaryOp(USub(), v):
-                tmp = Name(generate_name("tmp"))
-                (v_exp, v_temp) = self.rco_exp(v, True)
+                (v_exp, v_temp) = self.rco_exp(v, need_atomic)
 
-                return (tmp, [*v_temp, (tmp, UnaryOp(USub(), v_exp))])
+                if need_atomic:
+                    tmp = Name(generate_name("tmp"))
+                    return (tmp, [*v_temp, (tmp, UnaryOp(USub(), v_exp))])
+                else:
+                    return (UnaryOp(USub(), v_exp), v_temp)
             case BinOp(a, Add(), b):
-                (a_exp, a_temp) = self.rco_exp(a, True)
-                (b_exp, b_temp) = self.rco_exp(b, True)
+                (a_exp, a_temp) = self.rco_exp(a, need_atomic)
+                (b_exp, b_temp) = self.rco_exp(b, need_atomic)
 
                 # Turn result of binary op into another temporary
                 if need_atomic:
@@ -39,8 +42,8 @@ class Compiler:
                 else:
                     return (BinOp(a_exp, Add(), b_exp), [*a_temp, *b_temp])
             case BinOp(a, Sub(), b):
-                (a_exp, a_temp) = self.rco_exp(a, True)
-                (b_exp, b_temp) = self.rco_exp(b, True)
+                (a_exp, a_temp) = self.rco_exp(a, need_atomic)
+                (b_exp, b_temp) = self.rco_exp(b, need_atomic)
 
                 # Turn result of binary op into another temporary
                 if need_atomic:
@@ -90,16 +93,75 @@ class Compiler:
     ############################################################################
 
     def select_arg(self, e: expr) -> arg:
-        # YOUR CODE HERE
-        pass
+        match e:
+            case Constant(c):
+                return Immediate(c)
+            case Name(n):
+                return Variable(n)
+            case _:
+                raise Exception("Unsupported atomic expression: ", e)
 
     def select_stmt(self, s: stmt) -> list[instr]:
-        # YOUR CODE HERE
-        pass
+        match s:
+            case Expr(Call(Name("print"), [atm])):
+                return [
+                    Instr("movq", [self.select_arg(atm), Reg("rdi")]),
+                    Callq(label_name("print_int"), 1),
+                ]
+            case Expr(exp):
+                match exp:
+                    case Call(Name("input_int"), []):
+                        return [Callq(label_name("read_int"), 0)]
+                    case UnaryOp(USub(), atm):
+                        return [Instr("negq", [self.select_arg(atm)])]
+                    case BinOp(a, op, b):
+                        if str(op) == "+":
+                            op_instr = "addq"
+                        elif str(op) == "-":
+                            op_instr = "subq"
+                        else:
+                            raise Exception("Unsupported binary op in assign: ", s)
+
+                        a_arg, b_arg = self.select_arg(a), self.select_arg(b)
+                        return [
+                            Instr("movq", [a_arg, Reg("rax")]),
+                            Instr(op_instr, [b_arg, Reg("rax")]),
+                        ]
+                    case e:
+                        return [Instr("movq", [self.select_arg(e), Reg("rax")])]
+            case Assign([Name(var)], BinOp(atm_a, op, atm_b)) if str(
+                atm_a
+            ) == var or str(atm_b) == var:
+                # Handle special case  when `a` or `b` is `var`
+                if str(op) == "+":
+                    op_instr = "addq"
+                elif str(op) == "-":
+                    op_instr = "subq"
+                else:
+                    raise Exception("Unsupported binary op in assign: ", s)
+
+                a_arg, b_arg = self.select_arg(atm_a), self.select_arg(atm_b)
+                if str(a_arg) == var:
+                    return [Instr(op_instr, [b_arg, Variable(var)])]
+                else:
+                    return [Instr(op_instr, [a_arg, Variable(var)])]
+            case Assign([Name(var)], UnaryOp(USub(), atm)):
+                return [
+                    Instr("movq", [self.select_arg(atm), Variable(var)]),
+                    Instr("negq", [Variable(var)]),
+                ]
+            case Assign([Name(var)], expr):
+                expr_instr = self.select_stmt(Expr(expr))
+                return [*expr_instr, Instr("movq", [Reg("rax"), Variable(var)])]
 
     def select_instructions(self, p: Module) -> X86Program:
-        # YOUR CODE HERE
-        pass
+        match p:
+            case Module(body):
+                return X86Program(
+                    [instr for stmt in body for instr in self.select_stmt(stmt)]
+                )
+            case _:
+                pass
 
     ############################################################################
     # Assign Homes
